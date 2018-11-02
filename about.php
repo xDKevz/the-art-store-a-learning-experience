9e5499b326768bc4898d5c08672c52f772e78789
Lee Palisoc <cpali566@mtroyal.ca>
2018-11-02 21:55:03 +0000

    Added Basic About Page

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>About</title>
+        
+    </head>
+    <body>
+        <h1>Members</h1>
+        <div>
+            <h3>Link to Github Page</h3>
+            <a href="https://github.com/xDKevz/comp3512-assignment2.git">Github Link</a>
+        </div>
+        <div>
+            <h4>John Kevin Ruiz</h4>
+        </div>
+        <div>
+            <h4>Charlemagne Palisoc</h4>
+        </div>
+        <div>
+            <h4>Jeremiah Valiente</h4>
+        </div>
+        <div>
+            <h4>Rafael Angelo Pucut</h4>
+        </div>
+    </body>
+</html>
