749228070ddabb59b8c851a6dde533efa583e975
Lee Palisoc <cpali566@mtroyal.ca>
2018-11-09 23:59:51 +0000

    Rafael - Stylized about.php

--- about.php
+++ about.php
@@ -1,28 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"/>
         <title>Art Store - About</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/main.css" type="text/css" />
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
+    
     <body>
-        <!--<header class="global-header">-->
-        <!--    <div class="mainLogo"><img src="images/art_store_logo.png"/></div>-->
-        <!--    <nav class="global-nav">-->
-        <!--        <ul>-->
-        <!--            <li><a href="">Home</a></li>-->
-        <!--            <li><a href="">Artists</a></li>-->
-        <!--            <li><a href="">Paintings</a></li>-->
-        <!--            <li><a href="">Galleries</a></li>-->
-        <!--            <li><a href="">Genres</a></li>-->
-        <!--            <li><a href="">About</a></li>-->
-        <!--        </ul>-->
-        <!--    </nav>-->
-        <!--</header>-->
         <header>
             <div class="logo"><img src="images/art_store_logo.png" alt="art store logo"></div>
             <div class="navigation">
-                <div class="toggle"><i class="fa fa-bars" aria-hidden="true"></i></div>
+                <div class="toggle"><i class="fa fa-bars mBars" aria-hidden="true"></i></div>
                 <ul class="menu">
                     <li><a href="home.php">HOME</a></li>
                     <li><a href="arists.php">ARTISTS</a></li>
@@ -33,38 +23,41 @@
                 </ul>
             </div>
         </header>
-        <h1>About</h1>
-        <h2>Team Members</h2>
-        <div class="teamMembers">
-            <a href="https://github.com/xDKevz" target="_blank">
-                <div>
-                    <img src="images/memoji_kevin.png" alt="Kevin"/>
-                    <h4>John Kevin Ruiz</h4>
-                </div>
-            </a>
-            <a href="https://github.com/leepalisoc" target="_blank">
-                <div>
-                    <img src="images/memoji_lee.png" alt="Lee"/>
-                    <h4>Charlemagne Palisoc</h4>
-                </div>
-            </a>
-            <a href="https://github.com/jvali655" target="_blank">
-                <div>
-                    <img src="images/memoji_jeremiah.png" alt="Jeremiah"/>
-                    <h4>Jeremiah Valiente</h4>
-                </div>
-            </a>
-            <a href="https://github.com/Rancelot" target="_blank">
-                <div>
-                    <img src="images/memoji_rafael.png" alt="Rafael"/>
-                    <h4>Rafael Angelo Pucut</h4>
-                </div>
-            </a>
-        </div>
-        <div>
-            <h1>Repository</h1>
-            <a href="https://github.com/xDKevz/comp3512-assignment2.git" target="_blank">
-                <img src="images/GitHub_Logo.png" alt="GitHub"/></a>
-        </div>
+        <main class="about">
+            <h1>About</h1>
+            <h2>Team Members</h2>
+            <div class="teamMembers">
+                <a href="https://github.com/xDKevz" target="_blank">
+                    <div>
+                        <img src="images/memoji_kevin.png" alt="Kevin"/>
+                        <h4>John Kevin Ruiz</h4>
+                    </div>
+                </a>
+                <a href="https://github.com/leepalisoc" target="_blank">
+                    <div>
+                        <img src="images/memoji_lee.png" alt="Lee"/>
+                        <h4>Charlemagne Palisoc</h4>
+                    </div>
+                </a>
+                <a href="https://github.com/jvali655" target="_blank">
+                    <div>
+                        <img src="images/memoji_jeremiah.png" alt="Jeremiah"/>
+                        <h4>Jeremiah Valiente</h4>
+                    </div>
+                </a>
+                <a href="https://github.com/Rancelot" target="_blank">
+                    <div>
+                        <img src="images/memoji_rafael.png" alt="Rafael"/>
+                        <h4>Rafael Angelo Pucut</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="repo">
+                <h1>Repository</h1>
+                <a href="https://github.com/xDKevz/comp3512-assignment2.git" target="_blank">
+                    <img src="images/GitHub_Logo.png" alt="GitHub"/></a>
+            </div>
+        </main>
+        <script type="text/javascript" src="js/main.js"></script>
     </body>
 </html>
