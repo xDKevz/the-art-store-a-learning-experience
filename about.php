d3dc04f6981a86738fdb18d46859609928991b8a
Lee Palisoc <cpali566@mtroyal.ca>
2018-11-09 18:54:03 +0000

    Rafael - Inserted Memojis and Github Logo

--- about.php
+++ about.php
@@ -10,20 +10,24 @@
         <h1>Group Members:</h1>
         <div>
             <h4>John Kevin Ruiz</h4>
+            <img src="images/memoji_kevin.png" alt="Kevin_Memoji"/>
         </div>
         <div>
             <h4>Charlemagne Palisoc</h4>
+            <img src="images/memoji_lee.png" alt="Lee_Memoji"/>
         </div>
         <div>
             <h4>Jeremiah Valiente</h4>
+            <img src="images/memoji_jeremiah.png" alt="Jeremiah_Memoji"/>
         </div>
         <div>
             <h4>Rafael Angelo Pucut</h4>
+            <img src="images/memoji_rafael.png" alt="Rafael_Memoji"/>
         </div>
         
         <div>
             <h3>Link to Github Page</h3>
-            <a href="https://github.com/xDKevz/comp3512-assignment2.git" target="_blank">Github Link</a>
+            <a href="https://github.com/xDKevz/comp3512-assignment2.git" target="_blank"><img src="images/GitHub_Logo.png" alt="GitHub_Logo"/></a>
         </div>
     </body>
 </html>
