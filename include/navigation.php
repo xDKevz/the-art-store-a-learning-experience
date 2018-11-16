a6b09e60f80a6df792cdcb4c04599c5569c6956d
Lee Palisoc <cpali566@mtroyal.ca>
2018-11-16 20:55:30 +0000

    Kevin,Rafael - Worked on single-artist.php, single-genre.php, navigation icons etc.

--- include/navigation.php
+++ include/navigation.php
@@ -2,11 +2,12 @@
     <div class="navigation">
         <div class="toggle"><i class="fa fa-bars mBars" aria-hidden="true"></i></div>
             <ul class="menu">
-                <li><a href="home.php">HOME</a></li>
-                <li><a href="artist.php">ARTISTS</a></li>
-                <li><a href="galleries.php">GALLERIES</a></li>
-                <li><a href="genre.php">GENRES</a></li>
-                <li><a href="about.php">ABOUT US</a></li>
-                <li><a href="login.php">LOG-IN</a></li>
+                <li><a href="home.php"><i class="fa fa-home"></i> HOME</a></li>
+                <li><a href="artist.php"><i class="fa fa-paint-brush"></i> ARTISTS</a></li>
+                <li><a href="galleries.php"><i class="fa fa-image"></i> GALLERIES</a></li>
+                <li><a href="genre.php"><i class="fa fa-list-ul"></i> GENRES</a></li>
+                <li><a href="about.php"><i class="fa fa-info"></i> ABOUT US</a></li>
+                <li><a href="about.php"><i class="fa fa-heart"></i> FAVOURITES</a></li>
+                <li><a href="login.php"><i class="fa fa-sign-in"></i> LOG-IN</a></li>
             </ul>
 </div>
