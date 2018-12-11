5512133c0a975bd58f7507666c40b47943081cdc
Kevin Ruiz <jruiz277@mtroyal.ca>
2018-12-11 03:34:20 +0000

    Finished the About Page

--- about.php
+++ about.php
@@ -16,33 +16,35 @@
         <main class="about">
             <div class="teamMembers">
                 <h2>Team Members</h2>
-                <a href="https://github.com/xDKevz" target="_blank">
-                    <div>
-                        <img src="images/about/memoji_kevin.png" alt="Kevin"/>
-                        <h4>John Kevin Ruiz</h4>
-                    </div>
-                </a>
-                <a href="https://github.com/leepalisoc" target="_blank">
-                    <div>
-                        <img src="images/about/memoji_lee.png" alt="Lee"/>
-                        <h4>Charlemagne Palisoc</h4>
-                    </div>
-                </a>
-                <a href="https://github.com/jvali655" target="_blank">
-                    <div>
-                        <img src="images/about/memoji_jeremiah.png" alt="Jeremiah"/>
-                        <h4>Jeremiah Valiente</h4>
-                    </div>
-                </a>
-                <a href="https://github.com/Rancelot" target="_blank">
-                    <div>
-                        <img src="images/about/memoji_rafael.png" alt="Rafael"/>
-                        <h4>Rafael Angelo Pucut</h4>
-                    </div>
-                </a>
+                <div class="memojis">
+                    <a href="https://github.com/xDKevz" target="_blank">
+                        <div id="kevin">
+                            <img src="images/about/memoji_kevin.png" alt="Kevin"/>
+                            <h4>John Kevin Ruiz</h4>
+                        </div>
+                    </a>
+                    <a href="https://github.com/leepalisoc" target="_blank">
+                        <div id="lee">
+                            <img src="images/about/memoji_lee.png" alt="Lee"/>
+                            <h4>Charlemagne Palisoc</h4>
+                        </div>
+                    </a>
+                    <a href="https://github.com/jvali655" target="_blank">
+                        <div id="jeremiah">
+                            <img src="images/about/memoji_jeremiah.png" alt="Jeremiah"/>
+                            <h4>Jeremiah Valiente</h4>
+                        </div>
+                    </a>
+                    <a href="https://github.com/Rancelot" target="_blank">
+                        <div id="rafael">
+                            <img src="images/about/memoji_rafael.png" alt="Rafael"/>
+                            <h4>Rafael Angelo Pucut</h4>
+                        </div>
+                    </a>
+                </div>
             </div>
             <div class="repo">
-                <h1>Repository</h1>
+                <h2>Repository</h2>
                 <a href="https://github.com/xDKevz/comp3512-assignment2.git" target="_blank">
                     <img src="images/about/GitHub_Logo.png" alt="GitHub"/></a>
             </div>
